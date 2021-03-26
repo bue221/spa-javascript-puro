c1b5218fe68bb1c69eb2f0ccebad33cd0104bb50
bue221 <camiloplaza3@gmail.com>
2021-03-26 10:43:33 -0500

    webpack config files structurs some pages

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+export default () => {
+    const view = `
+    <div class='Characters'>
+     <arcticle class='Character-item'>
+        <a href='#/1/'>
+        <img src='image' alt='name' />
+        <h2>name</h2>
+        </a>
+     <article>
+    </div>
+    `;
+
+    return view;
+}
